a91397bf2cb7348fe767c0f215681e6e893a76f9
nikhilmmahajan <nikhil.mm@gmail.com>
2018-02-19 19:21:15 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/allPermutationsOfString.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/allPermutationsOfString.dir/main.cpp.obj"
+  "allPermutationsOfString.pdb"
+  "allPermutationsOfString.exe"
+  "allPermutationsOfString.exe.manifest"
+  "liballPermutationsOfString.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/allPermutationsOfString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void permute( string soFar, string rest)
+{
+    if(rest =="")
+        cout<<"\n"<<soFar;
+    else {
+        for (int i = 0; i < rest.size(); i++) {
+            string next = soFar + rest[i];
+            string remaining = rest.substr(0, i) + rest.substr(i + 1, rest.size());
+            permute(next, remaining);
+        }
+    }
+}
+
+
+int main() {
+    string s;
+    std::cout << "Hello, World! Enter the String to find its permutations " << std::endl;
+    cin >> s;
+    string empty ="";
+    permute(empty,s);
+
+    return 0;
+}
